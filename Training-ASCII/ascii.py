1e3eaa0e28b1fc4381c45d58779bc284475c390f
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2018-06-19 23:47:50 -0500

    first commit

---
+++ Training-ASCII/ascii.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Challenge
+
+# In a computer, you can only work with numbers.
+# In this challenge you have to decode the following message, which is in ASCII
+
+# Solution:
+
+codedMessage = "84, 104, 101, 32, 115, 111, 108, 117, 116, 105, 111, 110, 32, 105, 115, 58, 32, 114, 98, 102, 98, 108, 97, 101, 108, 110, 103, 102, 108"
+
